01f181ed9d3fc9c95fd3897e15ba3ac2d994ba7d
you5nesse <youn17esse@gmail.com>
2026-03-17 14:54:06 +0100

    Delete book-related exercises from main.js
    Removed exercises related to book inventory management.

--- main.js
+++ main.js
@@ -72,28 +72,3 @@ console.log(Object.entries(o));// [["x",10],["y",20]]
 const cop = Object.assign({}, o, { y: 30 });
 console.log(cop);
 console.log(o.hasOwnProperty("x"));
-/////partie 3//////
-const livres = [
-  { id: 1, titre: "Clean Code", auteur: "Robert C. Martin", annee: 2008, stock: 3, prix: 35.5 },
-  { id: 2, titre: "You Don’t Know JS", auteur: "Kyle Simpson", annee: 2015, stock: 0, prix: 28.0 },
-  { id: 3, titre: "Eloquent JavaScript", auteur: "Marijn Haverbeke", annee: 2018, stock: 5, prix: 32.9 },
-  { id: 4, titre: "JavaScript: The Good Parts", auteur: "Douglas Crockford", annee: 2008, stock: 2, prix: 22.0 },
-];
-//Exercice 3.1 
-const titres = livres.map(l => l.titre);
-const enStock = livres.filter(l => l.stock > 0);
-const valeurStock = livres.reduce((acc, l) => acc + l.prix * l.stock, 0);
-const plusAncien = livres.reduce((min, l) => (l.annee < min.annee ? l : min));
-const plusRecent = livres.reduce((max, l) => (l.annee > max.annee ? l : max));
-console.log({ titres, enStock, valeurStock, plusAncien, plusRecent });
-//Exercice 3.2
-const livre3 = livres.find(l => l.id === 3);
-const livresMaj = livres.map(l => (l.id === 2 ? { ...l, stock: l.stock + 1 } : l));
-console.log(livre3, livresMaj);
-//Exercice 3.3
-const parAnnee = livres.reduce((acc, l) => {
-  (acc[l.annee] ||= []).push(l);
-  return acc;
-}, {});
-console.log(parAnnee);
-
